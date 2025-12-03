ec5408ee73d9b6f1f19e5ab91c1a9680a4c15222
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-12-03 19:29:44 +0000

    - Add package: EFCore InMemory - Setup Swagger

--- Program.cs
+++ Program.cs
@@ -4,12 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add SwaggerUI and SwaggerGen functionality
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
