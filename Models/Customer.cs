54b021c827a2e2269f74df315a3a9ed0abd0f445
User-Desktop <User@DESKTOP-GS0OID3>
2025-12-03 21:06:32 +0000

    - Add customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace RestAPI_Minimal.Models;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string? CustomerNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+}
