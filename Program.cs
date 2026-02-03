dc547316b06b7389e9fceb1843cf8cfc9c928d46
User-Desktop <User@DESKTOP-GS0OID3>
2026-02-03 20:14:19 +0000

    Add dbcontext & sql server package

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using RestAPI_Minimal;
+using RestAPI_Minimal.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,10 @@ builder.Services.AddOpenApi();
 // Add SwaggerUI and SwaggerGen functionality
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Create DB context
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")
+    ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
 var app = builder.Build();
 
@@ -24,24 +32,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+// Endpoints
+
+
+app.MapGet("/Customers", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return new List<Customer>
+    {
+        new Customer
+        {
+            CustomerNumber = "CUST1",
+            FirstName = "Sukhpal",
+            LastName = "Shergill"
+        }
+    };
+
 })
-.WithName("GetWeatherForecast");
+.WithName("Customers");
 
 app.Run();
 
